ff53212047e14a65facac5dbd024e79ba43d4223
juanpabarriosr <32270243+juanpabarriosr@users.noreply.github.com>
2025-10-06 06:19:33 -0500

    funciona add pageobject

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,10 @@
+// Framework5/src/index.ts
+
 // Integración con Cucumber - World en español
 export { AutomatizacionWeb } from './cucumber/world/AutomatizacionWeb';
+
+// Clase base para PageObjects =====
+export { PageObject } from './pages/PageObject';
 
 // Helper para reportes
 export { generateTestReport } from './cucumber/hooks/reportHelper';
